47d29adac0f0790ebb5eaddb1e6ffd9acdc3b798
kevin_hws <kevinshu1995@gmail.com>
2021-08-25 11:51:54 +0800

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function App() {
-    return <div className="text-center"></div>;
+    return <div className="text-center">test</div>;
 }
 
 export default App;
